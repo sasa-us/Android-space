484d3f305c3e80a060eafecc0e3b5ad41e6daf82
sasa-us <sharryluh@gmail.com>
2018-12-03 18:00:32 -0800

    get input

--- DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
-        Log.i("Info", "button pressed !");
+        //get content of input
+        EditText myTextField = (EditText) findViewById(R.id.myTextField); //get all id in res find one id=myTextField
+        //convert view to editText
+        Log.i("Info", myTextField.getText().toString());
+//        Log.i("Info", "button is pressed");
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
